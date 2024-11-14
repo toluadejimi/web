fddfe359a9da7a0cedd207161a93132dc976abf2
Jimmy <toluadejimi@gmail.com>
2024-11-14 02:06:22 +0100

    mmkm

--- core/resources/views/templates/basic/user/payment/Enkpay.blade.php
+++ core/resources/views/templates/basic/user/payment/Enkpay.blade.php
@@ -1,7 +1,7 @@
 @extends($activeTemplate.'layouts.master')
 
 @section('content')
-    <div class="pt-60 pb-60 section-full" style="background: #f2f2f2;"> <!-- Background color added here -->
+    <div class="pt-60 pb-60 section-full" style="background: #f2f2f2;">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-6">
@@ -24,8 +24,20 @@
         </div>
     </div>
 
-    <!-- Centered iframe container with padding -->
-    <div id="iframeContainer" class="d-flex justify-content-center align-items-center mt-4" style="min-height: 500px; background: #f9f9f9;"></div>
+    <!-- Modal for iframe -->
+    <div class="modal fade" id="iframeModal" tabindex="-1" aria-labelledby="iframeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="iframeModalLabel">@lang('Payment')</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div id="iframeContainer" style="text-align: center;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('script')
@@ -37,14 +49,18 @@
             // Create iframe element
             const iframe = document.createElement("iframe");
             iframe.src = url;
-            iframe.width = "600";
-            iframe.height = "400";
+            iframe.width = "100%";
+            iframe.height = "500";
             iframe.style.border = "none";
 
             // Append iframe to container
             const container = document.getElementById("iframeContainer");
             container.innerHTML = ""; // Clear any existing iframes
             container.appendChild(iframe);
+
+            // Show the modal
+            var myModal = new bootstrap.Modal(document.getElementById('iframeModal'));
+            myModal.show();
         }
     </script>
 @endpush
